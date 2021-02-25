042e7b569181f141bd09c144e5aa375b69a151de
Jonatha <jkps.rs@gmail.com>
2021-02-25 14:48:01 -0300

    create survey structure

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 
 router.get("/surveys", surveysController.show);
 router.post("/surveys", surveysController.create);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
