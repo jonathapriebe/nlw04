6638d0e8b04c421b4b59388afa239c4d8354cff3
Jonatha <jkps.rs@gmail.com>
2021-02-26 14:03:12 -0300

    nps calculate

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 import { SendMailController } from './controllers/SendMailController';
 import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
@@ -10,6 +11,7 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 
@@ -19,5 +21,7 @@ router.post("/surveys", surveysController.create);
 router.post("/sendMail", sendMailController.execute);
 
 router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
